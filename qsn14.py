02bc822fa297c244c659de19a4948b0d81571a05
NairitaMitra <46235827+NairitaMitra@users.noreply.github.com>
2018-12-29 00:14:57 -0800

    QSN 14

---
+++ qsn14.py
@@ -0,0 +1,10 @@
+#qsn 14 submitted by NAIRITA MITRA
+
+
+from collections import Counter
+z=[]
+x=int(input("Enter the number of inputs u want to give"))
+for y in range(x):
+    a=int(input("Enter the number"))
+    z.append(a)
+Counter(z)
